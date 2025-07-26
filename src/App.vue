58360031ccf6c127627f79e198e5e8eba70d4d50
david111691 <davidchaikovsky100@gmail.com>
2025-07-26 23:38:36 +0300

    all other files

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AccountManagementForm from "./components/AccountManagementForm.vue";
+</script>
+
+<template>
+  <AccountManagementForm />
+</template>
+
+<style scoped></style>
